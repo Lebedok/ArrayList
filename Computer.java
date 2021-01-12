6e118c8f08098175fb13940bff432c37ed443546
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2021-01-11 22:43:05 -0600

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,29 @@
+package ArrayList;
+
+public class Computer {
+
+    String manufacturer;
+    String name;
+    Integer price;
+    Double screenSize;
+
+    public  Computer(String manufacturer, String  name, Integer price, Double screenSize){
+        this.manufacturer = manufacturer;
+        this.name = name;
+        this.price = price;
+        this.screenSize = screenSize;
+
+    }
+
+    public Computer(String manufacturer, String name, Double screenSize){
+        this.manufacturer = manufacturer;
+        this.name = name;
+        this.screenSize = screenSize;
+    }
+
+    public String toString(){
+        return this.manufacturer + " " + this.name + " " + this.price + " " + this.screenSize;
+    }
+
+}
+
